d203807a5648f17d446993c4737f282115ae0ea0
paghapour <pouriaaghapour@gmail.com>
2023-09-18 10:17:45 +0330

    update blog app

--- blog/admin.py
+++ blog/admin.py
@@ -5,5 +5,6 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'status', 'datetime_modified', ]
-    ordering = ['status', ]
+    list_display = ('title', 'status', 'datetime_modified',)
+    ordering = ('status', )
+
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.shortcuts import get_object_or_404
+from .models import Post
+from django.contrib.auth.models import User
+from .forms import NewPostForm
+from django.views import generic
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class PostListView(generic.ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        return Post.objects.filter(status='pub').order_by('-datetime_modified')
+
+
+class PostDetailView(generic.DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+
+class PostCreateView(generic.CreateView):
+    form_class = NewPostForm
+    template_name = 'blog/post_create.html'
+
+
+class PostUpdateView(generic.UpdateView):
+    model = Post
+    form_class = NewPostForm
+    template_name = 'blog/post_create.html'
+
+
+class PostDeleteView(generic.DetailView):
+    model = Post
+    template_name = 'blog/post_delete.html'
+    success_url = reverse_lazy('post_list')
